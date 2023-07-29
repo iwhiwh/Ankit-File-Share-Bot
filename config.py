7a22e370b5d6759f8bc1d0a6708c2bc9227736ff
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-07-29 11:22:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌😡Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1942168587 1197918807)
+ADMINS.append()
 
 LOG_FILE_NAME = "DigitalRelease.txt"
 
